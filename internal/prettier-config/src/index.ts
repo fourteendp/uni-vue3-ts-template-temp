86161c336cdf2cd63250e497aff54bb16d947992
FourteenDP <FourteenDP@qq.com>
2023-07-05 10:33:50 +0800

    🌈 style: tab使用四个空格

--- internal/prettier-config/src/index.ts
+++ internal/prettier-config/src/index.ts
@@ -1,18 +1,19 @@
 import { Config } from "prettier";
 
 const config: Config = {
-  printWidth: 120,
-  trailingComma: "all",
-  htmlWhitespaceSensitivity: "strict",
-  plugins: ["prettier-plugin-packagejson"],
-  overrides: [
-    {
-      files: ".*rc",
-      options: {
-        parser: "json",
-      },
-    },
-  ],
+    printWidth: 120,
+    tabWidth: 4,
+    trailingComma: "all",
+    htmlWhitespaceSensitivity: "strict",
+    plugins: ["prettier-plugin-packagejson"],
+    overrides: [
+        {
+            files: ".*rc",
+            options: {
+                parser: "json",
+            },
+        },
+    ],
 };
 
 export default config;
